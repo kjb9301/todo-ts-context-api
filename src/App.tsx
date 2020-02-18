993eae50aa92e995c1e0485014581793f98ea602
kjb9301 <kjb9301@naver.com>
2020-02-18 17:00:58 +0900

    install @types/styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
 
 function App() {
-  return <div className='App'>react</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <div className='App'>react</div>
+    </>
+  );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #e9ecef;
+  }
+`;
 
 export default App;
