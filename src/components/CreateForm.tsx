da80d8fd3bca6215e3cf20b929d38e0694280274
jbkang <kjb9301@github.com>
2020-02-19 22:24:59 +0900

    publishing createForm component

--- src/components/CreateForm.tsx
+++ src/components/CreateForm.tsx
@@ -4,14 +4,13 @@ import styled from 'styled-components';
 function CreateForm() {
   return (
     <Form>
-      <Input type='text' placeholder='할 일을 입력 후, Enter를 누르세요' />
+      <Input type='text' name='todo' placeholder='할 일을 입력 후, Enter를 누르세요' />
     </Form>
   );
 }
 
 const Form = styled.form`
   width: 90%;
-  /* height: 30px; */
 `;
 
 const Input = styled.input`
@@ -20,6 +19,8 @@ const Input = styled.input`
   border-radius: 4px;
   border: 1px solid #dee2e6;
   padding: 12px;
+  box-sizing: border-box;
+  font-size: 18px;
 `;
 
 export default CreateForm;
