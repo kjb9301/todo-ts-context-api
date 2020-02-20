0d2fb5d2287973095ec79c28b9a9e3e38606e7e9
kjb9301 <kjb9301@naver.com>
2020-02-20 14:56:06 +0900

    install react-icons & publishing TodoItem component

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import TodoItem from './TodoItem';
+
+const todoList = [
+  { id: 0, text: '숨쉬기', done: true },
+  { id: 1, text: '개발하기', done: false },
+  { id: 2, text: '잠자기', done: false }
+];
+
 function TodoList() {
-  return <Wrapper>list</Wrapper>;
+  return (
+    <Wrapper>
+      {todoList.map(todo => {
+        return <TodoItem key={todo.id} todo={todo} />;
+      })}
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-  border: 1px solid red;
-  height: calc(100% - 170px);
+  height: calc(100% - 210px);
+  padding: 20px;
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
